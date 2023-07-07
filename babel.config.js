b80125f28e962b78da817ad97ecd033d408fef44
Sebastian Markbåge <sebastian@calyptus.eu>
2023-07-07 11:09:45 -0400

    [Flight] Client and Server Reference Creation into Runtime (#27033)

--- babel.config.js
+++ babel.config.js
@@ -25,3 +25,4 @@ module.exports = {
     ['@babel/plugin-transform-block-scoping', {throwIfClosureRequired: true}],
   ],
 };
+
--- fixtures/legacy-jsx-runtimes/babel.config.js
+++ fixtures/legacy-jsx-runtimes/babel.config.js
@@ -10,3 +10,4 @@ module.exports = {
   ],
   plugins: ['@babel/plugin-transform-modules-commonjs'],
 };
+
